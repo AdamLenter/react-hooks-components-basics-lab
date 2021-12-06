0e5fa2776aeec67f2e9f8c7516f031c2a39134f3
Adam <adamlenter1@gmail.com>
2021-12-06 12:08:44 -0500

    commit tree 6462cb199cd8f1779391134afafa6d75dbd878ee

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,15 @@ function Home() {
   );
 }
 
+
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id = "about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
@@ -25,6 +33,9 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home></Home>
+      <NavBar></NavBar>
+      <About></About>
     </div>
   );
 }
